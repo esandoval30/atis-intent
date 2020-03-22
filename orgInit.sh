7bda551dec76894a192a50624c66d5e196a5b5bb
esandoval30 <esandoval@salesforce.com>
2020-03-22 16:50:33 +0000

    Adding Intent Accuracy Package + Training Intent Model

--- orgInit.sh
+++ orgInit.sh
@@ -5,18 +5,19 @@ sfdx shane:permset:create -n ATIS_Admin -o ATIS__c
 sfdx force:source:push
 sfdx force:user:password:generate
 sfdx force:user:permset:assign --permsetname ATIS_Admin
-#Bulk ATIS utterance data
 sfdx force:data:bulk:upsert -s ATIS__c -f data/ATIS-bulk-load.csv -i Ext_Id__c
-#install EVL Playgound Package (version 1.22) & 
-sfdx force:package:install -p 04t0b000001oXjv 
-#Install Einstein Language Intent Accuracy Package
-#sfdx force:package:install -p ???
 
-#Provision EVL account & Configure the Playground
+#install EVL Playgound Package (version 1.22) & Einstein Language Intent Accuracy Package
+sfdx force:package:install -p 04t0b000001oXjv 
+sfdx force:package:install -p 04t4J000002AU1H
+
+#Sign up to free EVL account & configure the Playground to work with EVL
 #sfdx shane:heroku:repo:deploy -g mshanemc -r heroku-empty -n atis-intent -t autodeployed-demos
 sfdx shane:heroku:repo:deploy -g mshanemc -r heroku-empty -n `basename "${PWD/mshanemc-}" | awk -F'-' '{print "medical-appt-" $3}'` -t autodeployed-demos
 sfdx shane:ai:playground:setupHeroku --verbose -a `basename "${PWD/mshanemc-}" | awk -F'-' '{print "medical-appt-" $3}'` -k 
-#sfdx shane:ai:dataset:upload -f data/ATIS-intent-training.csv -n AtisDataset --train
+
+#Upload ATIS dataset and train an intent classification model on it
+sfdx shane:ai:dataset:upload -f data/ATIS-intent-training.csv -n AtisDataset --train
 
 #open the scratch org
 sfdx force:org:open -p /lightning/setup/SetupOneHome/home
