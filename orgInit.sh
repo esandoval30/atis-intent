7779f407436aade13f27ea459e5eac69e82536b8
esandoval30 <esandoval@salesforce.com>
2020-03-23 11:35:43 +0000

    wait for intent training

--- orgInit.sh
+++ orgInit.sh
@@ -18,7 +18,7 @@ basename "${PWD/mshanemc-}" | awk -F'-' '{print "atis-" $4}'
 sfdx shane:ai:playground:setupHeroku --verbose -a `basename "${PWD/mshanemc-}" | awk -F'-' '{print "atis-" $4}'` -k 
 
 #Upload ATIS dataset and train an intent classification model on it
-sfdx shane:ai:dataset:upload -f data/ATIS-intent-training.csv
+sfdx shane:ai:dataset:upload -f data/ATIS-intent-training.csv --verbose --wait=10
 
 #open the scratch org
 sfdx force:org:open -p /lightning/setup/SetupOneHome/home
